dceb433c2d2ddd560fcf6c9f5982f265697655e0
Brenden Padilla <85804361+brenden-js@users.noreply.github.com>
2023-11-09 15:14:09 -0800

    Removed meta data generation for now.

--- src/app/(marketing)/blog/[...slug]/page.tsx
+++ src/app/(marketing)/blog/[...slug]/page.tsx
@@ -18,7 +18,7 @@ interface PostPageProps {
   }
 }
 
-async function getPostFromParams(params) {
+function getPostFromParams(params: { slug: string[] }) {
   const slug = params?.slug?.join("/")
   const post = allPosts.find((post) => post.slugAsParams === slug)
 
@@ -29,61 +29,16 @@ async function getPostFromParams(params) {
   return post
 }
 
-export async function generateMetadata({
-  params,
-}: PostPageProps): Promise<Metadata> {
-  const post = await getPostFromParams(params)
-
-  if (!post) {
-    return {}
-  }
-
-  const url = env.NEXT_PUBLIC_APP_URL
-
-  const ogUrl = new URL(`${url}/api/og`)
-  ogUrl.searchParams.set("heading", post.title)
-  ogUrl.searchParams.set("type", "Blog Post")
-  ogUrl.searchParams.set("mode", "dark")
-
-  return {
-    title: post.title,
-    description: post.description,
-    authors: post.authors.map((author) => ({
-      name: author,
-    })),
-    openGraph: {
-      title: post.title,
-      description: post.description,
-      type: "article",
-      url: absoluteUrl(post.slug),
-      images: [
-        {
-          url: ogUrl.toString(),
-          width: 1200,
-          height: 630,
-          alt: post.title,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: post.title,
-      description: post.description,
-      images: [ogUrl.toString()],
-    },
-  }
-}
-
-export async function generateStaticParams(): Promise<
+export function generateStaticParams():
   PostPageProps["params"][]
-> {
+ {
   return allPosts.map((post) => ({
     slug: post.slugAsParams.split("/"),
   }))
 }
 
-export default async function PostPage({ params }: PostPageProps) {
-  const post = await getPostFromParams(params)
+export default  function PostPage({ params }: PostPageProps) {
+  const post =  getPostFromParams(params)
 
   if (!post) {
     notFound()
