70163e21f9564afa4cde431950664f33a1a15dc9
brenden-js <anomaly.brenden@gmail.com>
2024-02-15 13:18:21 -0800

    Cleaned up files.

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -36,7 +36,7 @@ interface CreateContextOptions {
  *
  * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
  */
-export const createInnerTRPCContext = async (opts: CreateContextOptions) => {
+export const createInnerTRPCContext = (opts: CreateContextOptions) => {
   return {
     headers: opts.headers,
     db,
